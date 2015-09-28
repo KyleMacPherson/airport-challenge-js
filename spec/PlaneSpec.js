ea321bff7afb9356160590e725ff217500ca7915
kyle macpherson <kyle1@kyles-MacBook-Pro.local>
2015-09-28 17:47:57 +0100

    passed second test, plane can land

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -1,12 +1,20 @@
 describe('Plane',function() {
-  
-  var plane;  
 
-  beforeEach(function() {
-    plane = new Plane();
-});
+  var plane;
+
+  // beforeEach(function() {
+  //   plane = new Plane();
+  // });
 
   it('flying when created', function() {
+    plane = new Plane;
     expect(plane.isFlying).toBe(true);
   });
+
+  it('is not flying when landed', function() {
+    plane = new Plane;
+    plane.land();
+    expect(plane.isFlying).toBe(false);
+  });
+
 });
